f99d8325e8d7627cb1df2bffa053ee771f1b3ece
Duong Van Phuc <duongvan.phuc@ucg.co.nz>
2023-06-30 18:17:50 +0700

    #11863: fix UI in iOS

---
+++ UCG.siteTRAXLite/UCG.siteTRAXLite/Behaviors/IOSFixInfiniteHeightCollectionView.cs
@@ -0,0 +1,32 @@
+﻿namespace UCG.siteTRAXLite.Behaviors
+{
+    public class IOSFixInfiniteHeightCollectionView : Behavior<CollectionView>
+    {
+        private void CollectionViewOnSizeChanged(object? sender, EventArgs e)
+        {
+#if IOS
+            if (sender is not CollectionView collectionView)
+            {
+                throw new InvalidOperationException();
+            }
+            if (collectionView.Handler?.PlatformView is not UIKit.UIView uiView)
+            {
+                throw new InvalidOperationException();
+            }
+
+            var uiCollectionView = uiView.Subviews
+                .OfType<UIKit.UICollectionView>()
+                .Single();
+
+            const double emptyViewRequiredHeight = 50;
+            const double additionalHeight = 20; 
+            var calculatedHeight = (double)uiCollectionView.CollectionViewLayout.CollectionViewContentSize.Height;
+            var height = additionalHeight + calculatedHeight;
+            collectionView.MaximumHeightRequest = Math.Max(emptyViewRequiredHeight, height);
+#endif
+        }
+
+        protected override void OnAttachedTo(CollectionView bindable) => bindable.SizeChanged += CollectionViewOnSizeChanged;
+        protected override void OnDetachingFrom(CollectionView bindable) => bindable.SizeChanged -= CollectionViewOnSizeChanged;
+    }
+}
